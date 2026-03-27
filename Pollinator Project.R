98e3c3383fcdaba335d16e845bb9eb268e1b0603
Conn0r-Co0k <cook.connor.3306@gmail.com>
2026-03-27 13:30:50 -0400

    Update Pollinator Project.R
    Updated for new approach of not splitting

--- Pollinator Project.R
+++ Pollinator Project.R
@@ -1,5 +1,4 @@
-setwd("C:/Users/gr00k/Downloads/Data Analysis/Pollinator Project")
-
+setwd("~/GitHub/My Castilleja Work")
 #load in relevant packages
 library(tidyverse)#for data wrangling and restructuring
 library(statmod)
@@ -22,6 +21,8 @@ library(dplyr)
 conflicts_prefer(dplyr::filter)
 conflicts_prefer(dplyr::lag)
 rlang::last_trace()
+conflicts_prefer(tidyr::extract)
+conflicts_prefer(purrr::setnames)
 
 
 
@@ -53,10 +54,10 @@ community %<>%
 
 
 #Create new data only with segments with castilleja
-network_yes <- filter(network, cast_presence == "1")
-network_no <- filter(network, cast_presence == "0")
-community_yes <- filter(community, cast_presence == "1")
-community_no <- filter(community, cast_presence == "0")
+#network_yes <- filter(network, cast_presence == "1")
+#network_no <- filter(network, cast_presence == "0")
+#community_yes <- filter(community, cast_presence == "1")
+#community_no <- filter(community, cast_presence == "0")
 
 
 #for the community data sets create a data set that is each segment and the number of plants in each segment
@@ -69,72 +70,38 @@ community_no_plants <- community_no %>%
   group_by(transect,segment) %>% 
   count(cast_presence)
 
-#-----------Looking at mean amount of plants according to the presence of castilleja---------#
-#find mean and standard deviation amongst the segments in each data set
-comm_yes_plant_mean <- community_yes_plants %>% 
-  group_by(transect) %>% 
-  summarise(mean = mean(n),
-            se = sd(n)/sqrt(n()))
-
-comm_no_plant_mean <- community_no_plants %>% 
-  group_by(transect) %>% 
-  summarise(mean = mean(n),
-            se = sd(n)/sqrt(n()))
-
-comm_no_plant_mean <- community_no_plants %>% 
-  group_by(transect, segment, cast_presence) %>% 
-  summarise()
-
-community_sums <- community %>% 
-  group
 
 
-#Graph mean amount of plants in each transect according to the presence of castilleja
-#Graph for transects with segments with castilleja
-ggplot(data = comm_yes_plant_mean, aes(x = transect, y =mean, fill = transect)) + #sets the data and the axes for what we would like in our plot
+ggplot(data = community_sums, aes(x = segment, y = n, fill = as.factor(cast_presence))) + #sets the data and the axes for what we would like in our plot
   geom_col(position = position_dodge(0.7), width = .6, linewidth = 0.75, alpha = 0.9, size = 0.1) + #position_dodge keeps bars from being on top of each other, alpha is opacity, line width is width of line around bars, width is width of bars
-  geom_errorbar(aes(ymin = mean-se, ymax = mean+se), #Sets error bars as going from mean-se to mean+se
-                position = position_dodge(width = 0.7), width = 0.15) + #Corrects and places error bars correctly
-  labs(x = "Transect", y = "Number of Plants") + #labels the axes
-  scale_fill_manual(values=c("#4b3b40","#b6ad90", "brown", "grey", "tan"))+ #sets the colors of the bars
-  theme_pubr() +
-  ylim(0,300) #sets y axis limit
-
-#Graph for transects with segments without castilleja
-ggplot(data = comm_no_plant_mean, aes(x = transect, y =mean, fill = transect)) + #sets the data and the axes for what we would like in our plot
-  geom_col(position = position_dodge(0.7), width = .6, linewidth = 0.75, alpha = 0.9, size = 0.1) + #position_dodge keeps bars from being on top of each other, alpha is opacity, line width is width of line around bars, width is width of bars
-  geom_errorbar(aes(ymin = mean-se, ymax = mean+se), #Sets error bars as going from mean-se to mean+se
-                position = position_dodge(width = 0.7), width = 0.15) + #Corrects and places error bars correctly
   labs(x = "Transect", y = "Number of Plants") + #labels the axes
   scale_fill_manual(values=c("#4b3b40","#b6ad90", "purple"))+ #sets the colors of the bars
   theme_pubr() +
   ylim(0,50) #sets y axis limit
 
-#-------Looking at total amount of flowers according to presence of castilleja------#
-comm_yes_flowers <- community_yes %>%
-  group_by(transect,segment) %>%
-  sum("total_flowers", na.rm = FALSE)
+
+community_sums$segment <- as.factor(community_sums$segment)
+
+facet_wrap(~transect)
+
+ggplot(data = community_sums, aes(x = segment, y = n, fill = cast_presence)) +
+  geom_col() +
+  theme_pubr() +
+  scale_fill_manual(values=c(1, 0))+ #sets the colors of the bars
+  
 
 
 
-#Now I want to look at the network data, from our graphs we can see the entire transect that has segments with no castilleja has a lower amount of plants on average
-#Create a new dataset that shows the count of insects of each order for each segment
-network_yes_insects <- network_yes %>% 
-  group_by(transect, segment) %>% 
-  count(insect_order)
-
-#Make a new variable that shows the total amount of insects per segment, for some reason it doesn't put it into the environment dataset byt prints it in the console
-network_yes_insects %>% 
-  group_by(transect, segment) %>% 
-  mutate(insect_total = sum(n))
 
 
-network_no_insects <- network_no %>% 
-  group_by(transect, segment) %>% 
-  count(insect_order)
 
-network_no_insects %>% 
-  group_by(transect, segment) %>% 
-  mutate(insect_total = sum(n))
+
+
+
+
+
+
+
+
          
 
